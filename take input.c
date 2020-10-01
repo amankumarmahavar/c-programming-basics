64bcba8695222bb8c7fdfe3ae5f3339620408cd9
amankumarmahavar <72187898+amankumarmahavar@users.noreply.github.com>
2020-10-01 14:45:08 +0530

    Update take input.c

--- take input.c
+++ take input.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-    int i,j,k;
-    printf("enter two values \n");
-    scanf("%d%d", &i, &j);
-    k=i+j;
-    printf("the sum of %d and %d is %d \n",i,j,k);
+    int num1,num2,sum;
+    printf("Enter two values to make summation \n");
+    scanf("%d%d", &num1, &num2);
+    sum=num1+num2;
+    printf("The sum of %d and %d is %d \n",num1,num2,sum);
 
     return 0;
 }
